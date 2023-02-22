86d86e57573c578ca4d4bfa41e05f1623f00a4a1
NanaKwasi <yeboahkwasi09@gmail.com>
2023-02-22 15:43:52 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Entry ponit
+ *  Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	int j;
+	for (j = 'a'; j <= 'z'; j++)
+	{
+		_putchar(j);
+	}
+	_putchar('\n');
+	return (0);
+}
