1c1532d57012c3ff3a7d4156e181401ff09238db
NanaKwasi <yeboahkwasi09@gmail.com>
2023-02-22 16:18:31 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+void 2-print_alphabet_x10(void);
+{
+	int j;
+	int x
+
+	for (x = 0; x > 10; x++)
+	{
+		_putchar(j);
+	}
+	_putchar('\n'0;
+	return 0;
+}
