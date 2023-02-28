f389c7a0198d00bad87be24623b811cb711546f7
NanaKwasi <yeboahkwasi09@gmail.com>
2023-02-28 04:01:11 -0800

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,17 +10,17 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	int '|' = 0;
+	int | = 0;
 	int x = 0;
 
-	while (*(src + '|') != '\0')
+	while (*(src + |) != '\0')
 	{
-		'|'++;
+		|++;
 	}
-	for ( ; x < '|' ; x++)
+	for ( ; x < | ; x++)
 	{
 		dest[x] = src[x];
 	}
-	dest['|'] = '\0';
+	dest[|] = '\0';
 	return (dest);
 }
