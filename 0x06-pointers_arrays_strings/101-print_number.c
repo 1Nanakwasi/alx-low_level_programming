6a05e8215e366f36f303d4a9fa2e1ab040511347
NanaKwasi <yeboahkwasi09@gmail.com>
2023-03-02 15:57:17 -0800

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,19 +9,19 @@
 
 void print_number(int n)
 {
-	unsigned int a1;
+	unsigned int n1;
 
-	a1 = n;
+	n1 = n;
 
 	if (n < 0)
 	{
 		_putchar('-');
-		a1 = -n;
+		n1 = -n;
 	}
 
-	if (a1 / 10 !=0)
+	if (n1 / 10 !=0)
 	{
-		print_number(a1 / 10);
+		print_number(n1 / 10);
 	}
-	_putchar((a1 % 10) + '0');
+	_putchar((n1 % 10) + '0');
 }
