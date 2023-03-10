3e827f33525325570e06e7de3add72fccdf4e04c
NanaKwasi <yeboahkwasi09@gmail.com>
2023-03-09 16:01:05 -0800

    5-sqrt_recursion.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,7 +1,5 @@
 #include "main.h"
 
-int actual_prime(int an, int ai);
-
 /**
  * is_prime_number - says if an integer is a prime number or not
  * @n: number to evaluate
@@ -9,27 +7,18 @@ int actual_prime(int an, int ai);
  * Return: 1 if n is a prime number, 0 if not
  */
 
-int is_prime_number(int an)
+int is_prime_number(int n)
 {
-	if (an <= 1)
+	if (n <= 1)
 		return (0);
-	return (actual_prime(an, an - 1));
+	return (actual_prime(n, n - 1));
 }
 
-/**
- * actual_prime - calculates if a number is prime recursively
- * @n: number to evaluate
- * @i: iterator
- *
- * Return: 1 if n is prime, 0 if not
- */
-
-int actual_prime(int an, int ai)
+int actual_prime(int n, int a)
 {
-	if (ai == 1)
+	if (a == 1)
 		return (1);
-	if (an % ai == 0 && ai > 0)
+	if (n % a == 0 && a > 0)
 		return (0);
-	return (actual_prime(an, ai - 1));
+	return (actual_prime(n, a - 1));
 }
-
